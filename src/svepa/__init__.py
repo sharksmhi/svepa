c5a7dc312419824c86183f1df50d02ae4a34b323
Wenzer Magnus <a001985@ad.smhi.se>
2024-05-07 11:51:43 +0200

    Remove svepa_information

--- src/svepa/__init__.py
+++ src/svepa/__init__.py
@@ -1,5 +1,1 @@
 from svepa.svepa import get_current_station_info
-from svepa.svepa_information import get_svepa_event
-from svepa.svepa_information import get_svepa_events
-from svepa.svepa_information import get_svepa_info
-from svepa.svepa_information import update_local_svepa_data
